0a53ef508edcad00e2d2d25cfcfea2efee64b713
tiakuv <tiakuv@gmail.com>
2020-05-05 23:46:52 +0300

    Commit 2

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
 import json
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return "Main page"
+
+@app.route('/goals/<goal>')
+def show_goal(goal):
+    return "It will be " + goal
+
+@app.route('/profiles/<id>')
+def show_prepod(id):
+    return "It will be " + id
+
+@app.route('/request')
+def show_req():
+    return ""
+
+@app.route('/request_done')
+def res_req():
+    return "It sended"
+
+@app.route('/booking/<id>/<day>/<time>')
+def book_form(id, day, time):
+    return "Brone form will be here"
+
+@app.route('/booking_done')
+def res_book():
+    return "Bokking succesful!"
