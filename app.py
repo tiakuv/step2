bbcec03fd6a3e06eafa07e22dd0f872811acc6db
tiakuv <tiakuv@gmail.com>
2020-05-10 01:41:31 +0300

    Commit 10 may

--- app.py
+++ app.py
@@ -2,17 +2,24 @@ import json
 from flask import Flask, render_template
 from random import sample
 from flask_wtf import FlaskForm
+from wtforms import StringField, RadioField
+from wtforms.validators import InputRequired, Email
 
 app = Flask(__name__)
 app.config.from_object('config')
-
-#class RequestForm(Form)
 
 with open("teachers.json", "r", encoding="utf-8") as f:
     teachers = json.load(f)
 
 with open("goals.json", "r", encoding="utf-8") as f:
     goals = json.load(f)
+
+class RequestForm(FlaskForm):
+
+    goal = RadioField('goal', choices=goals.items())
+    time = RadioField('time', choices=[(2 , "1-2 часа в неделю"),(5 , "3-5 часов в неделю"),(7 , "5-7 часов в неделю"),(10 , "7-10 часов в неделю")])
+    name = StringField('Вас зовут', [InputRequired()])
+    phone = StringField('Ваш телефон', [])
 
 @app.route('/')
 def main():
@@ -40,12 +47,23 @@ def show_prepod(id):
 
 @app.route('/request')
 def show_req():
-    output = render_template("request.html", goals=goals)
+    form = RequestForm()
+    output = render_template("request.html", form=form)
     return output
 
-@app.route('/request_done')
+@app.route('/request_done/', methods=["POST"])
 def res_req():
-    return "It sended"
+    form = RequestForm()
+
+    goal = form.goal.data
+    time = form.time.data
+    name = form.name.data
+    phone = form.phone.data
+
+    with open("request.json", "a", encoding="utf-8") as f:
+        json.dump({"goal": goal, "time": time, "name": name, "phone": phone}, f, indent=2, ensure_ascii=False)
+
+    return render_template("request_done.html", goal=goal, time=time, name=name, phone=phone)
 
 @app.route('/booking/<id>/<day>/<time>')
 def book_form(id, day, time):
