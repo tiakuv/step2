b8ef9ae24e54915321d9204f1d735a935e8f25df
tiakuv <tiakuv@gmail.com>
2020-05-14 00:45:30 +0300

    Commit 14 may

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from random import sample
 from flask_wtf import FlaskForm
 from wtforms import StringField, RadioField
 from wtforms.validators import InputRequired, Email
+import pprint
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -14,12 +15,31 @@ with open("teachers.json", "r", encoding="utf-8") as f:
 with open("goals.json", "r", encoding="utf-8") as f:
     goals = json.load(f)
 
+days = {"mon": "Понедельник",
+        "tue": "Вторник",
+        "wed": "Среда",
+        "thu": "Четверг",
+        "fri": "Пятница",
+        "sat": "Суббота",
+        "sun": "Воскресенье"}
+
 class RequestForm(FlaskForm):
 
     goal = RadioField('goal', choices=goals.items())
     time = RadioField('time', choices=[(2 , "1-2 часа в неделю"),(5 , "3-5 часов в неделю"),(7 , "5-7 часов в неделю"),(10 , "7-10 часов в неделю")])
     name = StringField('Вас зовут', [InputRequired()])
     phone = StringField('Ваш телефон', [])
+
+class BookingForm(FlaskForm):
+
+    name = StringField('Вас зовут', [InputRequired()])
+    phone = StringField('Ваш телефон', [])
+
+class TeacherBooking():
+    def __init__(self, id, day, time):
+        self.teacher = teachers[id]
+        self.day = day
+        self.time = time
 
 @app.route('/')
 def main():
@@ -37,12 +57,14 @@ def show_goal(goal):
         for tgoal in teacher['goals']:
             if tgoal == goal:
                 selected_teachers.append(teacher)
-    output = render_template("goal.html", teachers=selected_teachers)
+    goal = goals[goal].lower()
+    output = render_template("goal.html", teachers=selected_teachers, goal=goal)
     return output
 
 @app.route('/profiles/<id>')
 def show_prepod(id):
-    output = render_template("profile.html", teacher=teachers[int(id)])
+    teacher = teachers[int(id)]
+    output = render_template("profile.html", teacher=teacher, goals=goals, days=days)
     return output
 
 @app.route('/request')
@@ -67,7 +89,9 @@ def res_req():
 
 @app.route('/booking/<id>/<day>/<time>')
 def book_form(id, day, time):
-    return "Brone form will be here"
+    form = RequestForm()
+
+    return render_template("booking.html", form = form, day=day, days=days, time=time, teacher=teachers[int(id)])
 
 @app.route('/booking_done')
 def res_book():
