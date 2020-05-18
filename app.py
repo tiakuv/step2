b4ad2fe138fc0d5e3619ef71789b8bd4a2549bf2
tiakuv <tiakuv@gmail.com>
2020-05-18 03:10:06 +0300

    Commit 18 may

--- app.py
+++ app.py
@@ -1,45 +1,30 @@
 import json
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from random import sample
 from flask_wtf import FlaskForm
 from wtforms import StringField, RadioField, HiddenField
-from wtforms.validators import InputRequired, Email, Length
+from wtforms.validators import InputRequired, Regexp
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-with open("teachers.json", "r", encoding="utf-8") as f:
-    teachers = json.load(f)
 
-with open("goals.json", "r", encoding="utf-8") as f:
-    goals = json.load(f)
+def load_json(filename):
+    with open(f"data/{filename}", "r", encoding="utf-8") as f:
+        return json.load(f)
 
-with open("goals_emoji.json", "r", encoding="utf-8") as f:
-    goals_emoji = json.load(f)
 
-def writetoFile(filename, str_dict):
-    with open(f"data/{filename}", 'r+', encoding="utf-8") as file:
-        content = json.load(file)
-    content.append(str_dict)
-    with open(f"data/{filename}", 'w', encoding="utf-8") as file:
-        json.dump(content, file, indent=2, ensure_ascii=False)
-
-def read_json(filename):
+def save_new_data(filename, data):
     try:
-        with open(f"data/{filename}", 'r', encoding="utf-8") as file:
-            content = json.load(file)
+        with open(f"data/{filename}", "r", encoding="utf-8") as f:
+            content = json.load(f)
     except FileNotFoundError:
-        print("down to except")
-        file = open(f"data/{filename}", 'w+', encoding="utf-8")
-        json.dump([], file)
-        file.close()
         content = []
-    return content
+    content.append(data)
+    with open(f"data/{filename}", "w", encoding="utf-8") as f:
+        json.dump(content, f, indent=2, ensure_ascii=False)
 
-def write_json(filename, content):
-    with open(f"data/{filename}", 'w+', encoding="utf-8") as file:
-        json.dump(content, file, indent=2, ensure_ascii=False)
 
 days = {"mon": "Понедельник",
         "tue": "Вторник",
@@ -50,24 +35,37 @@ days = {"mon": "Понедельник",
         "sun": "Воскресенье"}
 
 have_time = {"1-2": "1-2 часа в неделю",
-            "3-5": "3-5 часов в неделю",
-            "5-7": "5-7 часов в неделю",
-            "7-10": "7-10 часов в неделю"}
+             "3-5": "3-5 часов в неделю",
+             "5-7": "5-7 часов в неделю",
+             "7-10": "7-10 часов в неделю"}
+
+
+def clear_goals(goals_):
+    g = {}
+    for t, info in goals_.items():
+        g[t] = info['name']
+    return g
+
+
+teachers = load_json("teachers.json")
+goals_emoji = load_json("goals_emoji.json")
+goals = clear_goals(goals_emoji)
 
 
 class RequestForm(FlaskForm):
     goal = RadioField('goal', choices=goals.items())
     time = RadioField('time', choices=have_time.items())
     name = StringField('Вас зовут', [InputRequired()])
-    phone = StringField('Ваш телефон', [Length(min=11, max=11, message="Должно быть 11 символов")])
-
+    phone = StringField('Ваш телефон', [InputRequired(), Regexp("([+][0-9]{12})$", message="Введите номер телефона")])
+    # не работают Length, Regexp
 
 class BookingForm(FlaskForm):
     time = HiddenField()
     day = HiddenField()
     id = HiddenField()
     name = StringField('Вас зовут', [InputRequired()])
-    phone = StringField('Ваш телефон', [])
+    phone = StringField('Ваш телефон', [InputRequired()])
+
 
 @app.route('/')
 def main():
@@ -75,8 +73,7 @@ def main():
     rand_list = sample(range(1, len(teachers)), 6)
     for i in rand_list:
         random_teachers.append(teachers[i])
-    output = render_template("index.html", teachers=random_teachers, goals=goals, goals_emoji=goals_emoji)
-    return output
+    return render_template("index.html", teachers=random_teachers, goals_emoji=goals_emoji)
 
 
 @app.route('/goals/<goal>')
@@ -86,21 +83,23 @@ def show_goal(goal):
         for g in teacher['goals']:
             if g == goal:
                 teachers_by_goal.append(teacher)
-    goal = goals[goal].lower()
-    output = render_template("goal.html", teachers=teachers_by_goal, goal=goal)
-    return output
+    # сортировка списка словарей по убыванию
+    teachers_by_goal = sorted(teachers_by_goal, key=lambda x: x["rating"], reverse=True)
+    goal = goals_emoji[goal]
+    return render_template("goal.html", teachers=teachers_by_goal, goal=goal)
+
 
 @app.route('/profiles/<int:id>')
 def show_profile(id):
     teacher = teachers[id]
-    output = render_template("profile.html", teacher=teacher, goals=goals, days=days)
-    return output
+    return render_template("profile.html", teacher=teacher, goals=goals_emoji, days=days)
+
 
 @app.route('/request')
 def show_req():
     form = RequestForm()
-    output = render_template("request.html", form=form)
-    return output
+    return render_template("request.html", form=form)
+
 
 @app.route('/request_done/', methods=["POST"])
 def res_req():
@@ -109,10 +108,12 @@ def res_req():
     time = form.time.data
     name = form.name.data
     phone = form.phone.data
-    with open("request.json", "a", encoding="utf-8") as f:
-        json.dump({"goal": goal, "time": time, "name": name, "phone": phone}, f, indent=2, ensure_ascii=False)
+
+    new_data = {"goal": goal, "time": time, "name": name, "phone": phone}
+    save_new_data("request.json", new_data)
+
     return render_template("request_done.html",
-                           goal=goals[goal], time=time, name=name, phone=phone)
+                           goal=goals[goal], time=have_time[time], name=name, phone=phone)
 
 
 @app.route('/booking/<int:id>/<day>/<time>')
@@ -121,7 +122,9 @@ def book_form(id, day, time):
     return render_template("booking.html",
                            form=form, teacher=teachers[id],
                            day=day, day_name=days[day], time=time)
-#не нравится, как передаю название дня недели и ключ. может можно это сделать красивее?
+
+
+# не нравится, как передаю название дня недели и ключ. может можно это сделать красивее?
 
 @app.route('/booking_done/', methods=["POST"])
 def res_book():
@@ -132,23 +135,12 @@ def res_book():
     time = form.time.data
     id = form.id.data
 
-    str = {"name": name, "phone": phone, "id": id, "day": day, "time": time}
-    """
-        content = read_json("booking.json")
-        content.append(str)
-        write_json("booking.json",content)
-    """
-    with open("data/booking.json", "r", encoding="utf-8") as f:
-        content = json.load(f)
-    content.append(str)
-    with open("data/booking.json", "a", encoding="utf-8") as f:
-        json.dump(content, f)
+    new_data = {"name": name, "phone": phone, "id": id, "day": day, "time": time}
+    save_new_data("request.json", new_data)
 
     return render_template("booking_done.html",
                            name=name, phone=phone,
                            day_name=days[day], time=time)
 
+
 app.run('localhost', 8000)
-
-
-
--- make_json.py
+++ make_json.py
@@ -1,8 +1,8 @@
 import json
 from data import goals, teachers
 
-with open ("goals.json", "w", encoding="utf-8") as f:
+with open ("data/goals_emoji.json", "w", encoding="utf-8") as f:
     json.dump(goals, f)
 
-with open ("teachers.json", "w", encoding="utf-8") as f:
+with open ("data/teachers.json", "w", encoding="utf-8") as f:
     json.dump(teachers, f, ensure_ascii=False)
